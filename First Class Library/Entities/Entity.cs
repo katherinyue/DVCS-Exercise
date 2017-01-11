a3be87688379bf462a8f42d57e81503e397f538a
Tongyue <tongyue94@gmail.com>
2017-01-11 13:16:33 -0700

    Set Entites

--- First Class Library/Entities/Entity.cs
+++ First Class Library/Entities/Entity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace First_Class_Library
+namespace First_Class_Library.Entities
 {
-    public class Class1
+    public class Entity
     {
     }
 }
